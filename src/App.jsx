817cd2c1c2a03e31466bff8c523be6530be85560
Md. Minar Munshi <minarmunshi46@gmail.com>
2022-09-11 01:17:26 +0600

    Structure and Layout

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import {
+  Billing,
+  Business,
+  Button,
+  CardDeal,
+  Clients,
+  CTA,
+  FeedbackCard,
+  Footer,
+  GetStarted,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonials,
+} from './components/index';
+import styles from './style';
+
+const App = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={` ${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={` ${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
